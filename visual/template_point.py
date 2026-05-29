6fc21d5748f8867d44a30f7404336bdcefdf9907
NickLempert <nick209s21@gmail.com>
2026-05-29 22:22:32 +0300

    Template points can change effects now.

--- visual/template_point.py
+++ visual/template_point.py
@@ -1,20 +1,34 @@
 import copy
 from typing import Type
 
+from visual.effects import Effect
 from visual.point import Point
 from visual.shapes import Shape
-from visual.transforms import Transforms, XShift, YShift
+from visual.transforms import Transforms, XShift, YShift, Rotation
 
 
 class TemplatePoint(Point):
-    def __init__(self, transforms: Transforms, active=True):
+    def __init__(self, transforms: Transforms,
+                 active=True,
+                 edge_type: Effect = None,
+                 background: Effect = None,
+                 background_rotation: Rotation = None):
         super().__init__(transforms[XShift].amount, transforms[YShift].amount)
         self.transforms = transforms
         self.active = active
+        self.edge_type = edge_type
+        self.background = background
+        self.background_rotation = background_rotation
 
     def get_shape(self, shape: Type[Shape] | Shape):
         if isinstance(shape, Shape):
             shape2 = copy.deepcopy(shape)
             shape2.transforms = self.transforms
+            if self.edge_type is not None:
+                shape2.edge_type = self.edge_type
+            if self.background is not None:
+                shape2.background = self.background
+            if self.background_rotation is not None:
+                shape2.background_rotation = self.background_rotation
             return shape2
         return shape(transforms=self.transforms)
